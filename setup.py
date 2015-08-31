e91178f3ddad87fbc55767b4067a22fb4d5ab1c9
Bagrat Aznauryan <bagrat@aznauryan.org>
2015-08-31 23:21:16 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,6 @@ import mutexinit
 from setuptools import setup
 
 setup(name='mutexinit',
-      version=mutexinit.__version__)
+      version=mutexinit.__version__,
+      author="Bagrat Aznauryan",
+      author_email="bagrat@aznauryan.org")
